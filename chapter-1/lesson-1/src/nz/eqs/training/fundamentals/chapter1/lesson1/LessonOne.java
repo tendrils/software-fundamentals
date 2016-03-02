48a89c442c0d89701a1024991a76e03e5d793bf9
Alex Fulton <alex@eqs.nz>
2016-03-02 14:03:04 +1300

    import minimal starting point

---
+++ chapter-1/lesson-1/src/nz/eqs/training/fundamentals/chapter1/lesson1/LessonOne.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.eqs.training.fundamentals.chapter1.lesson1;
+
+import nz.eqs.training.core.Lesson;
+
+/**
+ *
+ * @author Tendril
+ */
+public class LessonOne extends Lesson {
+
+    
+}
