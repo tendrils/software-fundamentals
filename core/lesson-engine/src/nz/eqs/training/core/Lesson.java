e3e059dc65cbd128344f55abb236834941d4fa45
Alex Fulton <alex@eqs.nz>
2016-03-03 15:27:45 +1300

    push progress upstream

--- core/lesson-engine/src/nz/eqs/training/core/Lesson.java
+++ core/lesson-engine/src/nz/eqs/training/core/Lesson.java
@@ -5,10 +5,25 @@
  */
 package nz.eqs.training.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Tendril
  */
 public abstract class Lesson {
+    protected List<Problem> problems;
     
+    protected Lesson() {
+        problems = new ArrayList<>();
+    }
+    
+    public static void main(String[] args) {
+        
+        Lesson lesson = new this();
+        for(Problem p: problems) {
+            
+        }
+    }
 }
